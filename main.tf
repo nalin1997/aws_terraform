05e428e2e4e179aad71da605423b9d0a00a754bd
root <root@ip-172-31-39-202.ap-south-1.compute.internal>
2025-10-07 12:00:04 +0530

    Initial Terraform AWS setup

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region= "ap-south-1"
+}
+resource "aws_instance" "demo_tf_instance" {
+    ami = "ami-0861f4e788f5069dd"
+    instance_type = "t2.medium"
+tag = {
+
+Name = "demo_tf_instance"
+
+
+}
+}
